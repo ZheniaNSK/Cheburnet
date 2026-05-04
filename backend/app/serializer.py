d0eef3598b60603e4cd3d811d4080f2ac94bb3ff
unknown <you@example.com>
2026-05-04 11:54:34 +0300

    add Massage serializer

--- backend/app/serializer.py
+++ backend/app/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from backend.app.models import User, Chat
+from backend.app.models import User, Chat, Message
 
 
 class RegisterSerializer(serializers.Serializer):
@@ -34,3 +34,18 @@ class ChatSerializer(serializers.ModelSerializer):
         ]
 
         extra_kwargs = {'user2': {'write_only': True}}
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = [
+            'id',
+            'user',
+            'chat',
+            'text',
+            'created_at',
+        ]
+        read_only_fields = [
+            'id',
+            'created_at',
+        ]
