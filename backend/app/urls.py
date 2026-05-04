4ef98a4220d32c6ba8ef3910d85ce5cc4c701950
unknown <you@example.com>
2026-05-04 13:36:20 +0300

    update urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSets, ChatViewSets, MassageViewSets
+from .views import UserViewSets, ChatViewSets
 router = DefaultRouter()
 router.register('auth', UserViewSets, 'auth')
 router.register('chat', ChatViewSets, 'chat')
-router.register('massage', MassageViewSets, 'massage')
 
 
 urlpatterns = [
