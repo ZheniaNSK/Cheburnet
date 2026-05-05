c69f7f495b57945caa472aeb513555ccdddf57a4
unknown <you@example.com>
2026-05-05 09:21:43 +0300

    add corsheaders

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,6 +29,14 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'app.User'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'app.authentication.BearerToken'
@@ -45,10 +53,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'app.apps.AppConfig'
+    'app.apps.AppConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
